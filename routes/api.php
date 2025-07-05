80eb8e0b255a7da50651580d1d3a2e92190e4433
Ghost <wahlolade@gmail.com>
2025-07-05 15:35:50 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuotationController;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
-    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+Route::group([ 'middleware' => 'api' ], function () {
+    Route::group([ 'prefix' => 'auth' ], function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+    });
 
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::post('/quotation', [QuotationController::class, 'generateQuotation'])->name('quotation.generate');
+    });
 });
