5c4498b33a355c6f21a5bbc19d91cf01bd63b617
Ghost <wahlolade@gmail.com>
2025-07-05 15:41:46 +0100

    update generateQuotation method return type

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Services\QuotationService;
 use App\Http\Requests\QuotationRequest;
 use App\Http\Resources\QuotationResource;
 
 class QuotationController extends Controller
 {
-    public function generateQuotation (QuotationRequest $request, QuotationService $quotationService): QuotationResource
+    public function generateQuotation (QuotationRequest $request, QuotationService $quotationService): QuotationResource | JsonResponse
     {
         try {
             $quotation = $quotationService->generate($request);
